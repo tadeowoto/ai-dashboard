45f4a13211dc222ca7b124274962883f66bf7df5
Tadeo Wotoszyn <tadeowotoszyn@MacBook-Pro-de-Tadeo.local>
2026-02-05 12:56:34 -0300

    layout styles, and shacn button added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { inter } from "@/ui/fonts";
+import { Button } from "@/components/ui/button";
 
 export const metadata: Metadata = {
   title: "Ai dashboard",
@@ -16,7 +17,20 @@ export default function RootLayout({
       <body
         className={`${inter.className} antialiased w-full h-screen bg-amber-100`}
       >
-        {children}
+        <main className="w-full h-full">
+          <aside className="w-1/6 h-full bg-amber-300 flex flex-col items-center py-10">
+            <div className="w-full h-30 text-center">
+              {/* Logo */}
+              <h1 className="text-2xl font-bold">Profile Settings</h1>
+            </div>
+            <div className="flex flex-col gap-3">
+              <Button>Proyects</Button>
+              <Button>Laboratory</Button>
+              <Button>History</Button>
+            </div>
+          </aside>
+          {children}
+        </main>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div>
-      <h1>Hello</h1>
-    </div>
-  );
+  return <div></div>;
 }
